2f1170bc396fb2b00f6d78af9901deef3b39d26b
Ivan <567817@muni.cz>
2025-04-08 16:51:36 +0200

    project init

---
+++ src/test/java/cz/inqool/tennisclub/TennisclubApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.inqool.tennisclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TennisclubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
