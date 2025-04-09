83f07aa1ea5ede52117404a8a1bcabc3a7c8e67d
Ivan <567817@muni.cz>
2025-04-09 19:39:50 +0200

    SurfaceType controller implemented

---
+++ src/main/java/cz/inqool/tennisclub/controller/SurfaceTypeController.java
@@ -0,0 +1,44 @@
+package cz.inqool.tennisclub.controller;
+
+
+import cz.inqool.tennisclub.dto.surfacetype.SurfaceTypeCreateDto;
+import cz.inqool.tennisclub.dto.surfacetype.SurfaceTypeDto;
+import cz.inqool.tennisclub.facade.SurfaceTypeFacade;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/surface-types")
+public class SurfaceTypeController {
+
+    @Autowired
+    SurfaceTypeFacade surfaceTypeFacade;
+
+    @PostMapping
+    public ResponseEntity<SurfaceTypeDto> createSurfaceType(@RequestBody @Valid SurfaceTypeCreateDto surfaceTypeCreateDto) {
+        SurfaceTypeDto createdSurfaceType = surfaceTypeFacade.createSurfaceType(surfaceTypeCreateDto);
+        return ResponseEntity.ok(createdSurfaceType);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<SurfaceTypeDto>> getAllSurfaceTypes() {
+        List<SurfaceTypeDto> surfaceTypes = surfaceTypeFacade.getAll();
+        return ResponseEntity.ok(surfaceTypes);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<SurfaceTypeDto> getSurfaceTypeById(@PathVariable Long id) {
+        SurfaceTypeDto surfaceType = surfaceTypeFacade.getById(id);
+        return ResponseEntity.ok(surfaceType);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteSurfaceType(@PathVariable Long id) {
+        surfaceTypeFacade.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+}
