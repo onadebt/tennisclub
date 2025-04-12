3e7aec9d704e2f81e40d28411c4efee856e1acef
Ivan <567817@muni.cz>
2025-04-12 18:21:23 +0200

    redundant exceptions removed

--- src/main/java/cz/inqool/tennisclub/exception/customer/CustomerAlreadyExistsException.java
+++ src/main/java/cz/inqool/tennisclub/exception/customer/CustomerAlreadyExistsException.java
@@ -5,8 +5,4 @@ public class CustomerAlreadyExistsException extends RuntimeException {
     public CustomerAlreadyExistsException(String phoneNumber) {
         super("Customer with phone number " + phoneNumber + " already exists.");
     }
-
-    public CustomerAlreadyExistsException(Long id) {
-        super("Customer with id " + id + " already exists.");
-    }
 }
--- src/main/java/cz/inqool/tennisclub/exception/reservation/ReservationNotFoundException.java
+++ src/main/java/cz/inqool/tennisclub/exception/reservation/ReservationNotFoundException.java
@@ -5,8 +5,4 @@ public class ReservationNotFoundException extends RuntimeException {
     public ReservationNotFoundException(Long id) {
         super("Reservation with ID " + id + " not found.");
     }
-
-    public ReservationNotFoundException(String message) {
-        super(message);
-    }
 }
